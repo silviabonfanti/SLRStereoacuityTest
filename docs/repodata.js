33683f7644d78b420dd85482e651af8f6b49b22e
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-09-17 14:19:48 +0200

    web site

---
+++ docs/repodata.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Number of papers:30</p>\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Number of journals:27</p>\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Number of contribution in proceedings:2</p>\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Number of books:0</p>\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Number of book chapters:1</p>\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Number of authors:116</p>\
+\
+<p xmlns:bibtex="http://bibtexml.sf.net/">Last update:2020-09-03T17:28:56.453+02:00</p>\
+\
+');
+		
